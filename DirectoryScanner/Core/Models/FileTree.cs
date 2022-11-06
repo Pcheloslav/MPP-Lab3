3dbb1639eb5ea8affed86a1166352af9c64df626
Pcheloslav <slav.malink@gmail.com>
2022-11-06 16:26:22 +0300

    initial commit

---
+++ DirectoryScanner/Core/Models/FileTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class FileTree
+    {
+        public Node Root { get; }
+        public FileTree(Node root)
+        {
+            Root = root;
+            Root.Length = GetLength(root);
+        }
+
+        private long GetLength(Node node)
+        {
+            if (node.Children == null)
+            {
+                return node.Length;
+            }
+
+            foreach (var child in node.Children)
+            {
+                node.Length += GetLength(child);
+            }
+            return node.Length;
+        }
+    }
+}
+
---
+++ DirectoryScanner/Core/Models/Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Node
+    {
+        public string FullName { get; }
+        public string Name { get; }
+        public long Length { get; set; }
+        public bool IsDirectory { get; }
+        public List<Node>? Children { get; set; }
+
+        public Node(string fullName, string name, bool isDirectory = false)
+        {
+            FullName = fullName;
+            Name = name;
+            IsDirectory = isDirectory;
+        }
+
+        public Node(string fullName, string name, long length) : this(fullName, name)
+        {
+            Length = length;
+        }
+
+        public Node(string fullName, string name, List<Node>? children) : this(fullName, name, true)
+        {
+            Children = children;
+        }
+    }
+}
